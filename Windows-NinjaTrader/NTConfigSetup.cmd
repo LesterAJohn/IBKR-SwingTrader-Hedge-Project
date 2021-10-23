917e7816c1e4398236c22ab1f6a2941b2a1c5284
Lester A John <LesterAJohn@aol.com>
2021-10-23 16:04:47 -0500

    Updated to add XML Configuration to NinjaTrader Module

---
+++ Windows-NinjaTrader/NTConfigSetup.cmd
@@ -0,0 +1,4 @@
+:: This moves the default AutoTradeConfig.xml file to the NinjaTrader 8 Document directory root
+
+mkdir "C:\\NinjaTraderConfig"
+copy /V /Y AutoTradeConfig.xml /B  "C:\\NinjaTraderConfig\\AutoTradeConfig.xml" /B
